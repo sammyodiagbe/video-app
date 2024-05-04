665447520a6b7911aa7f6d5a583cef4f1c3a3750
Samson Odiagbe <sam.odiagbe.dev@gmail.com>
2024-05-04 16:02:20 -0400

    authentication process

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -14,7 +14,6 @@ const SignupPage = () => {
   const [firstname, setFirstname] = useState("");
   const [lastname, setLastName] = useState("");
   const [pendingVerification, setPendingVerification] = useState(false);
-  const [code, setCode] = useState("");
 
   const createUserAccount = async (formData: FormData) => {
     if (!isLoaded) {
@@ -41,55 +40,58 @@ const SignupPage = () => {
   };
 
   return (
-    <>
-      {!pendingVerification ? (
-        <>
-          <h1 className="text-xl">Create your account</h1>
-          <form action={createUserAccount}>
-            <div>
-              <Input
-                value={firstname}
-                placeholder="Email"
-                onChange={(event) => setFirstname(event.target.value)}
-              />
-            </div>
-            <div>
-              <Input
-                value={lastname}
-                placeholder="Lastname"
-                onChange={(event) => setLastName(event.target.value)}
-              />
-            </div>
-            <div>
-              <Input
-                value={username}
-                placeholder="Username"
-                onChange={(event) => setUsername(event.target.value)}
-              />
-            </div>
-            <div>
-              <Input
-                value={email}
-                placeholder="Your email"
-                onChange={(event) => setEmail(event.target.value)}
-              />
-            </div>
-            <div>
-              <Input
-                value={password}
-                placeholder="Password"
-                onChange={(event) => setPassword(event.target.value)}
-              />
-            </div>
-            <div>
-              <Button>Create your account</Button>
-            </div>
-          </form>{" "}
-        </>
-      ) : (
-        <VerificationComponent email={email} />
-      )}
-    </>
+    <div className=" min-h-screen grid items-center justify-center">
+      <div className="w-[400px]">
+        {!pendingVerification ? (
+          <>
+            <h1 className="text-2xl mb-8">Create your account</h1>
+            <form action={createUserAccount} className="grid gap-[20px]">
+              <div>
+                <Input
+                  value={firstname}
+                  placeholder="Firstname"
+                  onChange={(event) => setFirstname(event.target.value)}
+                />
+              </div>
+              <div>
+                <Input
+                  value={lastname}
+                  placeholder="Lastname"
+                  onChange={(event) => setLastName(event.target.value)}
+                />
+              </div>
+              <div>
+                <Input
+                  value={username}
+                  placeholder="Username"
+                  onChange={(event) => setUsername(event.target.value)}
+                />
+              </div>
+              <div>
+                <Input
+                  value={email}
+                  placeholder="Your email"
+                  onChange={(event) => setEmail(event.target.value)}
+                />
+              </div>
+              <div>
+                <Input
+                  value={password}
+                  type="password"
+                  placeholder="Password"
+                  onChange={(event) => setPassword(event.target.value)}
+                />
+              </div>
+              <div>
+                <Button>Create your account</Button>
+              </div>
+            </form>{" "}
+          </>
+        ) : (
+          <VerificationComponent email={email} />
+        )}
+      </div>
+    </div>
   );
 };
 
