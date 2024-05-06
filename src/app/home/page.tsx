80887a9840d7ea55382cb429d7a0ccf9e89a23aa
Samson Odiagbe <sam.odiagbe.dev@gmail.com>
2024-05-06 17:26:50 -0400

    implementing authentication

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,7 +1,10 @@
+import { SignOutButton } from "@clerk/nextjs";
+
 const HomePage = async () => {
   return (
     <>
       <h1>Page should only be accessed by authenticated users,</h1>
+      <SignOutButton />
     </>
   );
 };
